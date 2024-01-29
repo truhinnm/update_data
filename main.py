6f9fc737b601dcad99b2d4b4a204b274edebc19d
truhinnm <nick_trush@mail.ru>
2024-01-29 22:42:37 +0300

    final prototype
    0 tests)

--- main.py
+++ main.py
@@ -28,6 +28,8 @@ def find_main_row(search_string, sheet, column):
         if cell.value is not None:
             if " ".join(cell.value.split()[-2:]) == search_string:
                 return str(cell.row)
+            if cell.value == search_string:
+                return str(cell.row)
 
 
 def edit_workbook(workbook, sheet, file_path, cell, data):
@@ -85,17 +87,50 @@ def transfer_to_main(filename, ot_workbook, ind_workbook, ind_val_workbook):
 
 
 if __name__ == '__main__':
-    davydov = 0
-    for root, dirs, files in os.walk(r"C:\Users\Amanat\Desktop\kpi formulas"):
+
+    # Открываем общие таблицы
+
+    main_path = r"C:\Users\nick_\Desktop\kpi formulas\Общая таблица"
+    m_tables = open_main_tables(main_path)
+    id_wb = load_workbook(m_tables["Исполнительская дисциплина.xlsx"], data_only=True)
+    td_wb = load_workbook(m_tables["Трудовая дисциплина.xlsx"], data_only=True)
+    ot_wb = load_workbook(m_tables["KPI общая таблица.xlsx"])
+
+    # Начинаем обработку индивидуальных листов
+
+    for root, dirs, files in os.walk(r"C:\Users\nick_\Desktop\kpi formulas"):
         for file in files:
             if file.endswith('.xlsx') and not file.startswith('~$'):
                 if ("KPI_Архив" not in root) & ("ДЕМО" not in root) & ("Общая таблица" not in root):
-                    if file == "Нуриев А.К..xlsx":
-                        davydov = 1
                     filepath = os.path.join(root, file)
-                    print(filepath)
-                    print(users[file][0] + " " + users[file][1])
-                    if davydov == 1:
-                        set_wb_pass(filepath, users[file][0], users[file][1])
+
+                    # Открываем индивидуальную таблицу
+
+                    cur_wb = decrypt_file(filepath, users[file][0])
+                    ind_wb = load_workbook(cur_wb)
+                    ind_val_wb = load_workbook(cur_wb, data_only=True)
+
+                    # Переносим данные
+
+                    transfer_data(id_wb, "Оценка", ind_wb, file, "Исполнительская дисциплина")
+                    transfer_data(td_wb, "Статистика", ind_wb, file, "Трудовая дисциплина")
+                    if file == "Якупов Альберт.xlsx":
+                        transfer_to_main("Производитель работ Якупов А.А..xlsx", ot_wb, ind_wb, ind_val_wb)
+                    if file == "Якупов А.А..xlsx":
+                        transfer_to_main("Инженер ПТО Якупов А.А..xlsx", ot_wb, ind_wb, ind_val_wb)
+                    transfer_to_main(file, ot_wb, ind_wb, ind_val_wb)
+
+                    # Сохраняем индивидуальный лист
+
+                    ind_wb.save(filepath)
+                    set_wb_pass(filepath, users[file][0], users[file][1])
+
+    # Сохраняем общую таблицу
+
+    ot_wb.save(os.path.join(main_path, "KPI общая таблица.xlsx"))
+    set_wb_pass(os.path.join(main_path, "KPI общая таблица.xlsx"),
+                main_tables["KPI общая таблица.xlsx"][0],
+                main_tables["KPI общая таблица.xlsx"][1])
+
 
 
